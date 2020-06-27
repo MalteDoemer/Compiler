150f2f8e444a23f2bc32c1384cee9966d1b7ea7e
Malte Dömer <maltedoemer@gmail.com>
2020-06-27 09:35:39 +0200

    added test for += -= -- ++ and so on

--- Compiler/src/Binding/BoundTree/BoundNode.cs
+++ Compiler/src/Binding/BoundTree/BoundNode.cs
@@ -6,26 +6,4 @@ namespace Compiler.Binding
     {
         public abstract TextSpan Span { get; }
     }
-
-    // internal sealed class BoundAdditionalAddignmentExpression : BoundExpression
-    // {
-    //     public BoundAdditionalAddignmentExpression(VariableSymbol variable, BoundBinaryOperator op, BoundExpression expression, TypeSymbol resultType, TextSpan identifierSpan, TextSpan equalSpan)
-    //     {
-    //         Variable = variable;
-    //         Op = op;
-    //         Expression = expression;
-    //         ResultType = resultType;
-    //         IdentifierSpan = identifierSpan;
-    //         EqualSpan = equalSpan;
-    //     }
-
-    //     public override TypeSymbol ResultType { get; }
-    //     public override TextSpan Span => IdentifierSpan + Expression.Span;
-
-    //     public TextSpan IdentifierSpan { get; }
-    //     public TextSpan EqualSpan { get; }
-    //     public VariableSymbol Variable { get; }
-    //     public BoundBinaryOperator Op { get; }
-    //     public BoundExpression Expression { get; }
-    // }
 }
