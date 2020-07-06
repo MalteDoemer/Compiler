c4059385306afa47de235f7dd00aa630d5102d1b
Malte Dömer <maltedoemer@gmail.com>
2020-07-06 22:13:34 +0200

    organized SyntaxTokenKind

--- Compiler/src/Syntax/SyntaxTokenKind.cs
+++ Compiler/src/Syntax/SyntaxTokenKind.cs
@@ -2,6 +2,7 @@ namespace Compiler.Syntax
 {
     public enum SyntaxTokenKind
     {
+        // Special
         End,
         Invalid,
         Int,
@@ -10,61 +11,77 @@ namespace Compiler.Syntax
         Identifier,
         Comment,
         Space,
-
-        SingleQuote,
-        DoubleQuote,
-
+        
         True,
         False,
+
+        //Single operators
         Plus,
         Minus,
         Star,
         Slash,
+        Percentage,
+        Ampersand,
+        Pipe,
+        Hat,
+        Tilde,
+        Bang,
+
+
+        // Double operators
         StarStar,
         SlashSlah,
+        PlusPlus,
+        MinusMinus,
+        PipePipe,
+        AmpersandAmpersand,
+
+        // Comparison operators
         LessThan,
         GreaterThan,
         EqualEqual,
         NotEqual,
         LessEqual,
         GreaterEqual,
-        Tilde,
-        Percentage,
-        Ampersand,
-        Pipe,
-        Hat,
-        PipePipe,
-        AmpersandAmpersand,
+
+        // Assignment operators
         Equal,
-        PlusPlus,
-        MinusMinus,
         PlusEqual,
         MinusEqual,
         StarEqual,
         SlashEqual,
         AmpersandEqual,
         PipeEqual,
+
+        // Brackets
         LParen,
         RParen,
         LCurly,
         RCurly,
-        Bang,
+
+        // Sperators
+        Comma,
+        Colon,
+
+        // Type keywords
         FloatKeyword,
         BoolKeyword,
         StringKeyword,
         IntKeyword,
+        AnyKeyword,
+        VoidKeyword,
+
+        // Declaring keywords
         VarKeyword,
+        ConstKeyword,
+        FunctionDefinitionKeyword,
+
+        // Control flow keywords
         IfKeyword,
         ElseKeyword,
         ForKeyword,
         WhileKeyword,
-        Comma,
-        VoidKeyword,
         DoKeyword,
-        AnyKeyword,
-        Colon,
-        FunctionDefinitionKeyword,
-        ConstKeyword,
         ContinueKeyword,
         BreakKewyword,
     }
