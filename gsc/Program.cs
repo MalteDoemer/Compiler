3d4e6bf1bf1111aee9a516be9d0cb6da9f1b290d
Malte Dömer <maltedoemer@gmail.com>
2020-07-12 19:26:01 +0200

    basics to emit a program

--- gsc/Program.cs
+++ gsc/Program.cs
@@ -4,43 +4,95 @@ using System.IO;
 using Compiler;
 using Compiler.Text;
 using System.Collections.Generic;
+using Mono.Options;
 
 namespace gsc
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            if (args == null || args.Length == 0)
-                Console.Out.ColorWrite("[ERROR]: no paths provided", ConsoleColor.Red);
-            else
+            var outputPath = (string)null;
+            var moduleName = (string)null;
+            var referencePaths = new List<string>();
+            var sourcePaths = new List<string>();
+            var needsHelp = false;
+
+            var options = new OptionSet(){
+                "usage: gsc <source-paths> [options]",
+                "",
+                {"r=", "The {path} of a assembly to referenc", r => referencePaths.Add(r) },
+                {"o=", "The {path} of the output file", o => outputPath = o },
+                {"m=", "The {name} of the module", m => moduleName = m },
+                {"<>", s => sourcePaths.Add(s)},
+                {"?|h|help", "Displays Help", h => needsHelp = true},
+                "",
+            };
+
+            try
             {
-                var paths = new List<string>();
-                foreach (var arg in args)
-                {
-                    if (File.Exists(arg))
-                        paths.Add(arg);
-                    else if (Directory.Exists(arg))
-                        paths.AddRange(Directory.EnumerateFiles(arg));
-                    else
-                        Console.Out.ColorWrite($"[ERROR]: The path <{arg}> is not a valid file or directory.");
-                }
-                InterpretFiles(paths);
+                options.Parse(args);
             }
+            catch (OptionException e)
+            {
+                WriteError(e.Message);
+                return -1;
+            }
+
+            if (needsHelp)
+            {
+                options.WriteOptionDescriptions(Console.Out);
+                return 0;
+            }
+
+            var paths = GetAllPaths(sourcePaths);
+            if (paths == null) return -1;
+            if (paths.Length == 0)
+            {
+                WriteError("No source files are provided.");
+                return -1;
+            }
+
+            if (outputPath == null)
+                outputPath = Path.ChangeExtension(paths[0], ".exe");
+
+            if (moduleName == null)
+                moduleName = Path.GetFileNameWithoutExtension(outputPath);
+
+            var sourceTexts = paths.Select(p => new SourceText(File.ReadAllText(p), p)).ToArray();
+            var compilation = Compilation.Compile(sourceTexts);
+            var diagnostics = compilation.Emit(moduleName, outputPath, referencePaths.ToArray());
+            diagnostics.WriteTo(Console.Out);
+
+            if (!diagnostics.HasErrors)
+                Console.Out.ColorWrite($"Sucessfully created {outputPath}\n", ConsoleColor.Green);
+
+            return 0;
         }
 
-        private static void InterpretFiles(IEnumerable<string> paths)
+        private static void WriteError(string err)
         {
-            var sourceTexts = new List<SourceText>();
+            Console.Error.WriteLine();
+            Console.Error.ColorWrite($"ERROR: {err}", ConsoleColor.Red);
+            Console.Error.WriteLine();
+        }
+
+        private static string[] GetAllPaths(IEnumerable<string> paths)
+        {
+            var result = new List<string>(paths.Count());
+
             foreach (var path in paths)
             {
-                var text = File.ReadAllText(path);
-                sourceTexts.Add(new SourceText(text, path));
+                if (File.Exists(path))
+                    result.Add(path);
+                else
+                {
+                    WriteError($"path {path} is not a file or directory.");
+                    return null;
+                }
             }
-
-            var compilation = Compilation.Compile(sourceTexts.ToArray());
-            compilation.Diagnostics.WriteTo(Console.Out);
-            compilation.Evaluate();
+            return result.ToArray();
         }
+
     }
 }
